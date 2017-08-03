ef524fe02ded3ca9e4153ab917f23a8b3c165081
v.vitovtsev <vlad.vitovtsev@mail.ru>
2017-08-03 22:13:45 +0700

    setting gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp'),
     sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-    return gulp.src('resources/assets/sass/style.sass')
+    return gulp.src('resources/assets/sass/*.sass')
         .pipe(sass())
-        .pipe(gulp.dest('public/css'));
+        .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('resources/assets/sass/*.sass', ['sass']); // Наблюдение за sass файлами
+    // Наблюдение за другими типами файлов
 });
