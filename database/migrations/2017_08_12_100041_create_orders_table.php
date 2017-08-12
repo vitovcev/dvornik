57633e898737a578caebcc52b13bda0b224f925e
v.vitovtsev <vlad.vitovtsev@mail.ru>
2017-08-12 17:07:38 +0700

    create orders table and model

---
+++ database/migrations/2017_08_12_100041_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('client');
+            $table->string('product');
+            $table->string('color')->nullable();
+            $table->integer('count');
+            $table->integer('price_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
